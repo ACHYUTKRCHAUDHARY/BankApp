1a67112fc0eb1fe7df2a8f28a17f0dd43083d385
achyut <achyutchaudhary47@gmail.com>
2026-01-12 21:04:39 +0530

    main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,33 @@
 package app;
+import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    static void main() {
+        System.out.println("Welcome to Console Bank");
+        Scanner scanner = new Scanner(System.in);
+       boolean running = true;
+       while (running) {
+           //when we have to do multiple lines in the java , then we have to use """ """ strings
+           System.out.println(""" 
+              1) Open Account
+              2) Deposit
+              3) Withdraw
+              4) Transfer
+              5) Account Statement
+              6) List Accounts
+              7) Search Accounts by Customer Name
+              0) Exist  
+           """);
+           System.out.println("Choose your choice: ");
+           String choice = scanner.nextLine().trim();
+           System.out.println("Your choice: " + choice);
+
+           switch (choice) {
+               case "1"
+           }
+       }
+
+
+    }
 }
